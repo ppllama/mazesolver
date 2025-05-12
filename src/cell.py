483c82fd9848136bb48fa1c7f55e0599d52e96f2
ppllama <neelapanchakan@gmail.com>
2025-05-12 18:58:15 +0530

    ch1l7

--- src/cell.py
+++ src/cell.py
@@ -1,4 +1,5 @@
 from graphics import Line, Point
+import time
 
 
 class Cell:
@@ -12,6 +13,9 @@ class Cell:
         self.has_right_wall = True
         self.has_top_wall = True
         self.has_bottom_wall = True
+
+    def __repr__(self):
+        return f"{self._x1, self._x2, self._y1, self._y2}"
 
     def draw(self, x1, y1, x2, y2):
         if self._win is None:
@@ -39,3 +43,53 @@ class Cell:
             fill_color = "gray"
         line = Line(Point(abs(self._x1 + self._x2) // 2, abs(self._y1 + self._y2) // 2), Point(abs(to_cell._x1 + to_cell._x2) // 2, abs(to_cell._y1 + to_cell._y2) // 2))
         self._win.draw_line(line, fill_color)
+
+
+
+class Maze:
+    def __init__(
+        self,
+        x1,
+        y1,
+        num_rows,
+        num_cols,
+        cell_size_x,
+        cell_size_y,
+        win,
+    ):
+        self._x1 = x1
+        self._y1 = y1
+        self._num_rows = num_rows
+        self._num_cols = num_cols
+        self._cell_size_x = cell_size_x
+        self._cell_size_y = cell_size_y
+        self._win = win
+        self._create_cells()
+
+    def _create_cells(self):
+        self._cells = []
+        for i in range(self._num_cols):
+            row = []
+            for j in range(self._num_rows):
+                cell = Cell(self._win)
+                row.append(cell)
+            self._cells.append(row)
+        
+        for i in range(len(self._cells)):
+            for j in range(len(self._cells[i])):
+                self._draw_cell(i, j)
+        
+        print(self._cells)
+        
+    def _draw_cell(self, i, j):
+
+        x1 = self._x1 + self._cell_size_x * i
+        y1 = self._y1 + self._cell_size_y * j
+        x2 = x1 + self._cell_size_x
+        y2 = y1 + self._cell_size_y
+        self._cells[i][j].draw(x1, y1, x2, y2)
+        self._animate()
+
+    def _animate(self):
+        self._win.redraw()
+        time.sleep(0.005)
--- src/main.py
+++ src/main.py
@@ -1,32 +1,34 @@
 from graphics import Window, Point, Line
-from cell import Cell
+from cell import Cell, Maze
 from tkinter import Tk
 
 def main():
     win = Window(800, 600)
-    c1 = Cell(win)
-    c1.has_right_wall = False
-    c1.draw(50, 50, 100, 100)
+    # c1 = Cell(win)
+    # c1.has_right_wall = False
+    # c1.draw(50, 50, 100, 100)
 
-    c2 = Cell(win)
-    c2.has_left_wall = False
-    c2.has_bottom_wall = False
-    c2.draw(100, 50, 150, 100)
+    # c2 = Cell(win)
+    # c2.has_left_wall = False
+    # c2.has_bottom_wall = False
+    # c2.draw(100, 50, 150, 100)
 
-    c1.draw_move(c2)
+    # c1.draw_move(c2)
 
-    c3 = Cell(win)
-    c3.has_top_wall = False
-    c3.has_right_wall = False
-    c3.draw(100, 100, 150, 150)
+    # c3 = Cell(win)
+    # c3.has_top_wall = False
+    # c3.has_right_wall = False
+    # c3.draw(100, 100, 150, 150)
 
-    c2.draw_move(c3)
+    # c2.draw_move(c3)
 
-    c4 = Cell(win)
-    c4.has_left_wall = False
-    c4.draw(150, 100, 200, 150)
+    # c4 = Cell(win)
+    # c4.has_left_wall = False
+    # c4.draw(150, 100, 200, 150)
 
-    c3.draw_move(c4, True)
+    # c3.draw_move(c4, True)
+
+    m1 = Maze(x1=50, y1=70, num_rows=50, num_cols=400, cell_size_x=10, cell_size_y=10, win=win)
 
     win.wait_for_close()
 
